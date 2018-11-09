f67d1086c7435f7f3749a370ff82df126efb31e6
Gevorg <gevo618@gmail.com>
2018-11-09 15:57:36 +0400

    -

--- src/ServicesFnc/Fnc.php
+++ src/ServicesFnc/Fnc.php
@@ -8,16 +8,6 @@ use GuzzleHttp\Client;
 class Fnc extends FncAbstract
 {
 
-    private $client;
-
-    protected static $QA_API_URL = 'https://uat.appraisalport.com/api/';
-
-    protected static $QA_TOKEN_URL = 'https://uat.appraisalport.com/token';
-
-
-
-
-
     /**
      * @param $username
      * @param $password
@@ -26,9 +16,8 @@ class Fnc extends FncAbstract
      */
     public static function login($username, $password, $grantType = 'password')
     {
-        return $this->loginCall($username, $password, $grantType);
+        return self::loginCall($username, $password, $grantType);
     }
-
 
 
     /**
@@ -38,158 +27,148 @@ class Fnc extends FncAbstract
      */
     public static function  fileRequest($accessToken, $orderId)
     {
-        $headers = [
-            'Authorization' => 'Bearer ' . $accessToken,
-            'Accept'        => 'application/json',
-        ];
-
-        $response = $this->client->request('GET', self::$QA_API_URL.'orders/'.$orderId.'/files', [
-            'headers' => $headers
-        ]);
-
-        $fileRequestResponse = $response->getBody()->getContents();
-        return $fileRequestResponse;
+        return self::callFileRequest($accessToken, $orderId);
     }
 
-
-
-    /**
-     * @param $accessToken
-     * @param $orderId
-     * @return \SimpleXMLElement
-     */
-    public static function snapshotRequest($accessToken, $orderId)
-    {
-        $headers = [
-            'Authorization' => 'Bearer ' . $accessToken,
-            'Accept'        => 'application/json',
-        ];
-
-        $response = $this->client->request('GET', self::$QA_API_URL.'orders/'.$orderId.'/propertyDataSnapshot', [
-            'headers' => $headers
-        ]);
-
-        $content = $response->getBody()->getContents();
-        $string = json_decode($content);
-        $xml = simplexml_load_string($string);
-        return $xml;
-    }
-
-
-    /**
-     * @param $accessToken
-     * @param $transactionId
-     * @return mixed
-     */
-    public static function onHoldStatus($accessToken, $serviceProviderId, $orderId, $reasonCode)
-    {
-        $headers = [
-            'Authorization' => 'Bearer ' . $accessToken,
-            'Accept'        => 'application/json',
-        ];
-
-        $response = $this->client->request('GET', self::$QA_API_URL.'serviceproviders/'.$serviceProviderId.'/orders/'.$orderId.'/onHold/'.$reasonCode, [
-            'headers' => $headers
-        ]);
-
-        $content = $response->getBody()->getContents();
-        return $content;
-    }
-
-
-    /**
-     * @param $accessToken
-     * @param $serviceProviderId
-     * @param $orderId
-     * @return mixed
-     */
-    public static function 	offHoldStatus($accessToken, $serviceProviderId, $orderId)
-    {
-        $headers = [
-            'Authorization' => 'Bearer ' . $accessToken,
-            'Accept'        => 'application/json',
-        ];
-
-        $response = $this->client->request('GET', self::$QA_API_URL.'serviceproviders/'.$serviceProviderId.'/orders/'.$orderId.'/offHold', [
-            'headers' => $headers
-        ]);
-
-        $content = $response->getBody()->getContents();
-        return $content;
-    }
-
-
-    /**
-     * @param $accessToken
-     * @param $serviceProviderId
-     * @param $orderId
-     * @param $comment
-     * @param $committedDate
-     * @return mixed
-     */
-    public static function orderPastDue($accessToken, $serviceProviderId, $orderId, $comment, $committedDate)
-    {
-        $headers = [
-            'Authorization' => 'Bearer ' . $accessToken,
-            'Content-Type'        => 'application/x-www-form-urlencoded',
-        ];
-
-        $formParams = [
-            'Comments' => $comment,
-            'CommittedDate' => $committedDate,
-        ];
-
-        $response = $this->client->request('POST', self::$QA_API_URL.'serviceproviders/'.$serviceProviderId.'/orders/'.$orderId.'/reportPastDue', [
-            'headers' => $headers,
-            'form_params' => $formParams
-        ]);
-        $content = $response->getBody()->getContents();
-        return $content;
-    }
-
-
-    /**
-     * @param $accessToken
-     * @param $orderId
-     * @return mixed
-     */
-    public static function getEglAsHtml($accessToken, $orderId)
-    {
-        $headers = [
-            'Authorization' => 'Bearer ' . $accessToken,
-            'Content-Type'        => 'application/x-www-form-urlencoded',
-        ];
-
-        $response = $this->client->request('GET', self::$QA_API_URL.'orders/'.$orderId.'/egl', [
-            'headers' => $headers
-        ]);
-
-        $content = $response->getBody()->getContents();
-        $data = json_decode($content);
-        return $data;
-    }
-
-
-    /**
-     * @param $accessToken
-     * @param $orderId
-     * @return mixed
-     */
-    public static function getEglAsPdf($accessToken, $orderId)
-    {
-        $headers = [
-            'Authorization' => 'Bearer ' . $accessToken,
-            'Content-Type'        => 'application/x-www-form-urlencoded',
-        ];
-
-
-        $response = $this->client->request('GET', self::$QA_API_URL'.orders/'.$orderId.'/EngagementLetter/Pdf', [
-        'headers' => $headers
-    ]);
-		$content = $response->getBody()->getContents();
-
-		return $content;
-	}
+//
+//
+//    /**
+//     * @param $accessToken
+//     * @param $orderId
+//     * @return \SimpleXMLElement
+//     */
+//    public static function snapshotRequest($accessToken, $orderId)
+//    {
+//        $headers = [
+//            'Authorization' => 'Bearer ' . $accessToken,
+//            'Accept'        => 'application/json',
+//        ];
+//
+//        $response = $this->client->request('GET', self::$QA_API_URL.'orders/'.$orderId.'/propertyDataSnapshot', [
+//            'headers' => $headers
+//        ]);
+//
+//        $content = $response->getBody()->getContents();
+//        $string = json_decode($content);
+//        $xml = simplexml_load_string($string);
+//        return $xml;
+//    }
+//
+//
+//    /**
+//     * @param $accessToken
+//     * @param $transactionId
+//     * @return mixed
+//     */
+//    public static function onHoldStatus($accessToken, $serviceProviderId, $orderId, $reasonCode)
+//    {
+//        $headers = [
+//            'Authorization' => 'Bearer ' . $accessToken,
+//            'Accept'        => 'application/json',
+//        ];
+//
+//        $response = $this->client->request('GET', self::$QA_API_URL.'serviceproviders/'.$serviceProviderId.'/orders/'.$orderId.'/onHold/'.$reasonCode, [
+//            'headers' => $headers
+//        ]);
+//
+//        $content = $response->getBody()->getContents();
+//        return $content;
+//    }
+//
+//
+//    /**
+//     * @param $accessToken
+//     * @param $serviceProviderId
+//     * @param $orderId
+//     * @return mixed
+//     */
+//    public static function 	offHoldStatus($accessToken, $serviceProviderId, $orderId)
+//    {
+//        $headers = [
+//            'Authorization' => 'Bearer ' . $accessToken,
+//            'Accept'        => 'application/json',
+//        ];
+//
+//        $response = $this->client->request('GET', self::$QA_API_URL.'serviceproviders/'.$serviceProviderId.'/orders/'.$orderId.'/offHold', [
+//            'headers' => $headers
+//        ]);
+//
+//        $content = $response->getBody()->getContents();
+//        return $content;
+//    }
+//
+//
+//    /**
+//     * @param $accessToken
+//     * @param $serviceProviderId
+//     * @param $orderId
+//     * @param $comment
+//     * @param $committedDate
+//     * @return mixed
+//     */
+//    public static function orderPastDue($accessToken, $serviceProviderId, $orderId, $comment, $committedDate)
+//    {
+//        $headers = [
+//            'Authorization' => 'Bearer ' . $accessToken,
+//            'Content-Type'        => 'application/x-www-form-urlencoded',
+//        ];
+//
+//        $formParams = [
+//            'Comments' => $comment,
+//            'CommittedDate' => $committedDate,
+//        ];
+//
+//        $response = $this->client->request('POST', self::$QA_API_URL.'serviceproviders/'.$serviceProviderId.'/orders/'.$orderId.'/reportPastDue', [
+//            'headers' => $headers,
+//            'form_params' => $formParams
+//        ]);
+//        $content = $response->getBody()->getContents();
+//        return $content;
+//    }
+//
+//
+//    /**
+//     * @param $accessToken
+//     * @param $orderId
+//     * @return mixed
+//     */
+//    public static function getEglAsHtml($accessToken, $orderId)
+//    {
+//        $headers = [
+//            'Authorization' => 'Bearer ' . $accessToken,
+//            'Content-Type'        => 'application/x-www-form-urlencoded',
+//        ];
+//
+//        $response = $this->client->request('GET', self::$QA_API_URL.'orders/'.$orderId.'/egl', [
+//            'headers' => $headers
+//        ]);
+//
+//        $content = $response->getBody()->getContents();
+//        $data = json_decode($content);
+//        return $data;
+//    }
+//
+//
+//    /**
+//     * @param $accessToken
+//     * @param $orderId
+//     * @return mixed
+//     */
+//    public static function getEglAsPdf($accessToken, $orderId)
+//    {
+//        $headers = [
+//            'Authorization' => 'Bearer ' . $accessToken,
+//            'Content-Type'        => 'application/x-www-form-urlencoded',
+//        ];
+//
+//
+//        $response = $this->client->request('GET', self::$QA_API_URL'.orders/'.$orderId.'/EngagementLetter/Pdf', [
+//        'headers' => $headers
+//    ]);
+//		$content = $response->getBody()->getContents();
+//
+//		return $content;
+//	}
 
 
 
--- src/ServicesFnc/core/FncAbstract.php
+++ src/ServicesFnc/core/FncAbstract.php
@@ -5,13 +5,19 @@ namespace ServicesFnc\core;
 
 abstract class FncAbstract
 {
-    private $client;
 
     protected static $QA_API_URL = 'https://uat.appraisalport.com/api/';
 
     protected static $QA_TOKEN_URL = 'https://uat.appraisalport.com/token';
 
-    protected function loginCall($username, $password, $grantType)
+
+    /**
+     * @param $username
+     * @param $password
+     * @param $grantType
+     * @return mixed
+     */
+    protected  static  function loginCall($username, $password, $grantType)
     {
         $params = array(
             'form_params' => array(
@@ -21,7 +27,7 @@ abstract class FncAbstract
             )
         );
 
-        $response =$this->call('POST', self::$QA_TOKEN_URL, $params);
+        $response = self::call('POST', self::$QA_TOKEN_URL, $params);
         $contents = $response->getBody()->getContents();
         $data = json_decode($contents);
         return $data->access_token;
@@ -30,19 +36,53 @@ abstract class FncAbstract
 
 
     /**
+     * @param $accessToken
+     * @param $orderId
+     * @return mixed
+     */
+    protected static function callFileRequest($accessToken, $orderId)
+    {
+        $headers = self::header($accessToken);
+        $response = self::call('GET', self::$QA_API_URL.'orders/'.$orderId.'/files', 'null', $headers);
+        $fileRequestResponse = $response->getBody()->getContents();
+        return $fileRequestResponse;
+    }
+
+
+
+
+    /**
      * @param $method
      * @param $url
      * @param array $params
      * @return string
      */
-    protected function call($method, $url, array $params)
+    protected static function call($method, $url, array $params=null, $header = null)
     {
         try {
-            $result = $this->client->$method($url, $params);
+            $client = new Client();
+            $result = $client->$method($url, $params, $header);
             return $result;
         } catch (\Throwable $e) {
             return $e->getMessage();
         }
+    }
+
+
+    /**
+     * @param $accessToken
+     * @return array
+     */
+    protected static function header($accessToken)
+    {
+        return [
+            'headers' => [
+                'Authorization' => 'Bearer ' . $accessToken,
+                'Accept' => 'application/json',
+            ]
+        ];
+
+
     }
 
 }
